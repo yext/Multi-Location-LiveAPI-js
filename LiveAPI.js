d20e83e22cf92900fd79af80f421237ca5801fcc
Kennan Murphy-Sierra <kennanmurphy@gmail.com>
2018-03-07 13:11:28 -0500

    Code Cleanup and Comments

--- LiveAPI.js
+++ LiveAPI.js
@@ -1,8 +1,12 @@
+
 // API Call
  $(document).ready(function () {
+  locationID = '8696490560696135898',
+  api_key = '92ca716c35bc76afb5684d5ab66fa95e',
+  dev_date = '20180305',
   $.ajax({
     type: 'GET',
-    url : 'https://liveapi.yext.com/v2/accounts/me/locations/8696490560696135898?api_key=92ca716c35bc76afb5684d5ab66fa95e&v=20180305',
+    url : 'https://liveapi.yext.com/v2/accounts/me/locations/' + locationID + '?api_key=' + api_key + '&v=' + dev_date,
     success: function(data, textStatus, xhr){
       renderPage(JSON.parse(xhr.responseText).response);
     }, error: function(xhr, textStatus, errorThrown) {
@@ -11,8 +15,10 @@
   });
 });
 
+// Populate information on the page
 function renderPage(answer) {
-  // document.getElementById("locationName").innerHTML = answer.locationName;
+  $("title").text(answer.locationName);
+  $(".navbar-brand").text(answer.locationName.substr(answer.locationName.indexOf(" ") + 1))
   $("#locationName").text(answer.locationName);
   $("#address").text(answer.address);
   $("#address2").text(answer.address2);
