7f3307eb660af92cb3f21eda179f3511270a932e
Kennan Murphy-Sierra <kennanmurphy@gmail.com>
2018-03-21 15:42:29 -0400

    Minor fixes from Code Review

--- LiveAPI.js
+++ LiveAPI.js
@@ -1,12 +1,11 @@
 
 // API Call
  $(document).ready(function () {
-  locationID = '8696490560696135898',
   api_key = '92ca716c35bc76afb5684d5ab66fa95e',
-  dev_date = '20180305',
+  vparam = '20180316',
   $.ajax({
     type: 'GET',
-    url : 'https://liveapi.yext.com/v2/accounts/me/locations/' + locationID + '?api_key=' + api_key + '&v=' + dev_date,
+    url : 'https://liveapi.yext.com/v2/accounts/me/locations' + '?api_key=' + api_key + '&v=' + vparam,
     success: function(data, textStatus, xhr){
       renderPage(JSON.parse(xhr.responseText).response);
     }, error: function(xhr, textStatus, errorThrown) {
@@ -15,23 +14,39 @@
   });
 });
 
-// Populate information on the page
+// Populate information on the page with information from Live API
 function renderPage(answer) {
-  $("title").text(answer.locationName);
-  $(".navbar-brand").text(answer.locationName.substr(answer.locationName.indexOf(" ") + 1))
-  $("#locationName").text(answer.locationName);
-  $("#address").text(answer.address);
-  $("#address2").text(answer.address2);
-  $("#city").text(answer.city);
-  $("#state").text(answer.state);
-  $("#zip").text(answer.zip);
-  $("#phone").text(formatPhoneNumber(answer.phone));
-  $("#phone").attr("href", "tel:" + answer.phone);
-  $("#featuredMessage").text(answer.featuredMessage);
-  $("#websiteUrl").attr("href", answer.websiteUrl);
-  $("#description").text(answer.description);
+
+  var html = '<div class="row card-deck">'
+  
+  for (i = 0; i < answer.count; i++) {
+    html += appendLocationCard(answer.locations[i]);
+  }
+
+  html += '</div>'
+  $("#placeholder").append(html);
 
   return;
+}
+
+// Create the HTML for each card
+function appendLocationCard(location) {
+
+  var locationHtml = '<div class="col-md-4">'
+  locationHtml += '<div class="card text-center mb-4">'
+
+  locationHtml += '<div class="card-body">'
+  locationHtml += '<h5 class="card-title">' + location.locationName + '</h5>'
+  locationHtml += '<p class="card-text">' + location.address
+  locationHtml += '<br>' + location.address2
+  locationHtml += '<br>' + location.city + ' '+ location.state + ' ' + location.zip + '</p>'
+  locationHtml += '<p>Call Us: <a href="tel:' + location.phone + '">' + formatPhoneNumber(location.phone) + '</a></p>'
+  locationHtml += '<a href=' + location.websiteUrl + ' class="btn btn-primary">Visit Website</a>'
+  locationHtml += '</div>'
+  
+  locationHtml += '</div></div>'
+
+  return locationHtml;
 }
 
 function formatPhoneNumber(s) {
@@ -39,10 +54,3 @@ function formatPhoneNumber(s) {
   var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
   return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
 }
-
-
-
-/********* 
-  Notes
-  document.getElementById("locationName").innerHTML = answer.locationName;
-**********/
